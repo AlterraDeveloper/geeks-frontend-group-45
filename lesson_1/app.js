3f675a3b769cf9080934093cfe67ec2eb9447c7d
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-12-12 21:15:27 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,15 @@
+// setTimeout(() => {
+//     document.body.style.backgroundColor = "green";
+// }, 3000);
+
+// document.body.style.fontSize = "108px";
+// let c = 100;
+// setInterval(() => {
+//     document.body.innerHTML = c--;
+// }, 1000);
+
+alert("Hello Group 45 ALERT");
+document.body.innerHTML = "Hello Group 45 HTML";
+console.log("Hello Group 45 DEVTOOLS");
+
+// JavaScript Engine - V8
